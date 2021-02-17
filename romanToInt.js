aa3e829b10bd1277511135812c20a5a8936126b3
Khairul Taher <khairultaher@outlook.com>
2021-02-17 15:04:08 +0600

    2021-02-17 1.4

---
+++ romanToInt.js
@@ -0,0 +1,28 @@
+
+  function romanToInt(string) {
+    const values = new Map([
+        ['I', 1],
+        ['V', 5],
+        ['X', 10],
+        ['L', 50],
+        ['C', 100],
+        ['D', 500],
+        ['M', 1000]
+        /*....*/
+      ]);
+    
+    let result = 0,
+      current, previous = 0;
+    for (const char of string.split("").reverse()) {
+      current = values.get(char);
+      if (current >= previous) {
+        result += current;
+      } else {
+        result -= current;
+      }
+      previous = current;
+    }
+    return result;
+  }
+
+console.log(romanToInt('LVIII'))
